2ffe1cb8d0c8f1c997d61107e9062476dd14aba2
oporpo73-sudo <oporpo73@gmail.com>
2025-11-26 14:43:48 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1764142170,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/player2-sheet0.png",
+		"images/gold-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/obj_turret-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
